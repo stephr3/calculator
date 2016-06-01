7fd7e085d0fef9e0124a42827a2508641b8ee405
cb and sg <sg@>
2016-06-01 11:24:27 -0700

    add functioning user interface for addition

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,31 @@
+// Business Logic
 var add = function(number1, number2) {
   return number1 + number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
+var sub = function(number1, number2) {
+  return number1 - number2;
+};
 
-alert(add(number1, number2));
+var mult = function(number1, number2) {
+  return number1 * number2;
+};
+
+var div = function(number1, number2) {
+  return number1 / number2;
+};
+
+var mod = function(number1, number2) {
+  return number1 % number2;
+};
+
+// User Interface
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+    $("#output").text(result);
+  });
+});
