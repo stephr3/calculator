093289580035de6c28da9b5ad86f249b1a85b1a9
Matt Wilkin and Stephanie Gurung <stephanie@>
2016-06-02 15:07:44 -0700

    add radio inline buttons to control calculator function

--- js/scripts.js
+++ js/scripts.js
@@ -21,11 +21,23 @@ var mod = function(number1, number2) {
 
 // User Interface
 $(document).ready(function() {
-  $("form#add").submit(function(event) {
+  $("form#calculate").submit(function(event) {
     event.preventDefault();
-    var number1 = parseInt($("#add1").val());
-    var number2 = parseInt($("#add2").val());
-    var result = add(number1, number2);
+    var number1 = parseInt($("#input1").val());
+    var number2 = parseInt($("#input2").val());
+    var operator = $("input:radio[name=operator]:checked").val();
+    var result;
+    if (operator === "add"){
+      result = add(number1, number2);
+    } else if (operator === "subtract") {
+      result = sub(number1, number2);
+    } else if (operator === "multiply") {
+      result = mult(number1, number2);
+    }else if (operator === "divide") {
+      result = div(number1, number2);
+    }else if (operator === "remainder") {
+      result = mod(number1, number2);
+    }
     $("#output").text(result);
     $("#output").show();
   });
